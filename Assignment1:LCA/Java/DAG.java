8aca1a3647d3a717be139ab1fd504d87a49152f7
Kai Suzuki <ifuymak@gmail.com>
2020-11-12 11:26:00 +0000

    add DAG for LCA

--- Assignment1:LCA/Java/DAG.java
+++ Assignment1:LCA/Java/DAG.java
@@ -8,72 +8,51 @@ public class DAG {
 	
 	public static class DAGNode{
 		int data;
-		ArrayList<DAGNode> children = new ArrayList<DAGNode>();
-		ArrayList<DAGNode> parents = new ArrayList<DAGNode>();
+		ArrayList<DAGNode> children ;
+		ArrayList<DAGNode> parents ;
 		
 		DAGNode(int number){
 			data = number;
-			
+			children = new ArrayList<DAGNode>();
+			parents = new ArrayList<DAGNode>();
 		}
 	}
 	
-	public void makeDAGNode(DAGNode root, int []parent_num, int num) {
-		if(root == null) root = new DAGNode(num);
-		else {
-			for(int i=0; i<root.children.size(); i++) {
-				for(int j=0; j<parent_num.length; j++) {
-					if(root.data == parent_num[j]) {
-						DAGNode newNode = new DAGNode(num);
-						newNode.parents.add(root);
-						root.children.add(newNode);
-					}
-				}
-				makeDAGNode(root.children.get(i), parent_num,num);
-			}
-		}
-	}
 	
-	public int findLCA(DAGNode root, int num1, int num2) {
-		int result = -1;
-		
-		
-		
-		for(int i=0; i<root.children.size();i++) {
-			if(root.children.get(i).data==num1) {
-				for(int j=0; j<root.children.size();j++) {
-					if(root.children.get(j).data==num2) result = root.data;
-				}
-			}
-		}
-		if(result!=-1) return result;
-		boolean matched = false;
-		for(int i=0; !matched && i<root.children.size();i++) {
-			result = findLCA(root.children.get(i),num1,num2);
-			if(result != -1) {
-				matched = true;
-			}
-		}
-		return result;
-	}
+
+	public  static DAGNode findLCA(DAGNode root, DAGNode a, DAGNode b){
+        if (root==null) return null;
+        if (root == a ||root == b) return root;
+        if(a.data==b.data)        return a;
+
+        ArrayList <DAGNode> lca = new ArrayList<DAGNode>();
+        if(a.parents.size()==0||b.parents.size()==0) return null;
+        for(int i=0; i<a.parents.size();i++){
+            for(int j=0;j<b.parents.size();j++){
+                if(a.parents.get(i).data==b.parents.get(j).data){
+                    lca.add(a.parents.get(i));
+                }
+            }
+
+        }
+
+        if(lca.size()==0){
+            if (a.data>b.data) lca.add(findLCA(root,a.parents.get(0), b));
+            else lca.add(findLCA(root,a,b.parents.get(0)));
+        }
+
+
+            DAGNode maxTemp = lca.get(0);
+
+            for (int i = 0; i < lca.size(); i++) {
+                if (lca.get(i).data > maxTemp.data) maxTemp = lca.get(i);
+            }
+            return maxTemp;
+
+
+
+    }
+
 	
-	public static void main(String[]args) {
-		DAG dag = new DAG();
-		int []par1 = {1};
-		int []par2 = {2,3};
-		int []par3 = {4};
-		
-		dag.makeDAGNode(dag.root, null, 1);
-		dag.makeDAGNode(dag.root,par1, 2);
-		dag.makeDAGNode(dag.root, par1, 3);
-		dag.makeDAGNode(dag.root,par2, 4);
-		dag.makeDAGNode(dag.root, par3, 5);
-		
-		int result2 = dag.findLCA(dag.root, 2,3);
-		assertEquals("result of 2 and 3: ","1",dag.findLCA(dag.root, 2,3));
-		assertEquals("result of 1 and 2: ","1",dag.findLCA(dag.root, 1,2));
-		dag = null;
-		assertEquals("result of 4 and 5: ","3",dag.findLCA(dag.root, 4,5));
-		System.out.println("result: "+result2);
-	}
 
 }
--- Assignment1:LCA/Java/unitTest_forLCA.java
+++ Assignment1:LCA/Java/unitTest_forLCA.java
@@ -10,6 +10,7 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+
 @RunWith(JUnit4.class)
 public class unitTest_forLCA {
 
@@ -40,24 +41,76 @@ public class unitTest_forLCA {
 	}
 	
 	@Test
-	public void test_DAG() {
-		DAG dag = new DAG();
-		int []par1 = {1};
-		int []par2 = {2,3};
-		int []par3 = {4};
+	public void test_for_Check_LCA_left() {
+		check_LCA test2 = new check_LCA();
 		
-		dag.makeDAGNode(dag.root, null, 1);
-		dag.makeDAGNode(dag.root,par1, 2);
-		dag.makeDAGNode(dag.root, par1, 3);
-		dag.makeDAGNode(dag.root,par2, 4);
-		dag.makeDAGNode(dag.root, par3, 5);
+		test2.makeTree("10 20 30 40 50 60 70 80");
 		
-		int result = dag.findLCA(dag.root, 2,3);
-		assertEquals("result of 2 and 3: ","1",dag.findLCA(dag.root, 2,3));
-		assertEquals("result of 1 and 2: ","1",dag.findLCA(dag.root, 1,2));
-		dag = null;
-		assertEquals("result of 4 and 5: ","4",dag.findLCA(dag.root, 4,5));
-		System.out.println("result: "+result);
+		Node result = new Node(test2.searchLCA(test2.allNodes.get(1), test2.allNodes.get(5)));
+		assertEquals("result of 2 and 6: ","20",String.valueOf(result.number));
+		
+		result = new Node(test2.searchLCA(test2.allNodes.get(2), test2.allNodes.get(4)));
+		assertEquals("result of 3 and 5: ","30",String.valueOf(result.number));
+		
+	
 	}
+	
+	
+
+	
+    @Test
+    public void testNull(){
+    	DAG.DAGNode testNode = null;
+        DAG.DAGNode a = new DAG.DAGNode(3);
+        DAG.DAGNode b= new DAG.DAGNode(4);
+        assertEquals("testingNull ",null,DAG.findLCA(testNode,a,b));
+
+    }
+
+    @Test
+    public void testSingleN0deTree(){
+    	DAG.DAGNode root = new DAG.DAGNode(1);
+    	DAG.DAGNode a = new DAG.DAGNode(1);
+    	DAG.DAGNode b = new DAG.DAGNode(1);
+    	assertEquals("if it is dingle node tree",1,DAG.findLCA(root,a,b).data);
+    }
+
+    @Test
+    public void testDAGNode() {
+    	DAG.DAGNode rootDAG = new DAG.DAGNode(1);
+    	DAG.DAGNode node2 = new DAG.DAGNode(2);
+    	DAG.DAGNode node3 = new DAG.DAGNode(3);
+    	DAG.DAGNode node4 = new DAG.DAGNode(4);
+    	DAG.DAGNode node5 = new DAG.DAGNode(5);
+        DAG.DAGNode node6 = new DAG.DAGNode(6);
+
+        rootDAG.children.add(node2);
+        rootDAG.children.add(node3);
+        rootDAG.children.add(node4);
+        rootDAG.children.add(node5);
+        rootDAG.children.add(node6);
+        node2.children.add(node4);
+        node2.parents.add(rootDAG);
+        node3.children.add(node4);
+        node3.children.add(node5);
+        node3.parents.add(rootDAG);
+        node4.children.add(node5);
+        node4.parents.add(node2);
+        node4.parents.add(node3);
+        node4.parents.add(rootDAG);
+        node5.parents.add(node3);
+        node5.parents.add(node4);
+        node5.parents.add(rootDAG);
+        node6.parents.add(node4);
+
+        assertEquals("result of DAG.findLCA(rootDAG, rootDAG, node4): ",1,DAG.findLCA(rootDAG, rootDAG, node4).data);
+        assertEquals("result of DAG.findLCA(null,null,null: ",null,DAG.findLCA(null,null,null));
+        assertEquals("result of DAG.findLCA(rootDAG, node6, node6): ",6, DAG.findLCA(rootDAG, node6, node6).data);
+        assertEquals("result of DAG.findLCA(rootDAG, node6, node6): ",3, DAG.findLCA(rootDAG, node4, node5).data);
+        assertEquals("result of DAG.findLCA(rootDAG, new DAG.DAGNode(9),new DAG.DAGNode(99)): ",null, DAG.findLCA(rootDAG, new DAG.DAGNode(9),new DAG.DAGNode(99)));
+
+
+
+    }
 
 }
